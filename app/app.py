78376138c6ebb3b1f3093e52bbf9eb3da1b33bfc
Guillermo Barroso <barrosoguillermo55@gmail.com>
2023-03-16 11:58:15 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/py')
 def hello():
-	return "Hello World!, My name is GBT55, this is my first zero-touch pipeline app.py"
+	return "Hello World! this is my first zero-touch pipeline app.py"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
